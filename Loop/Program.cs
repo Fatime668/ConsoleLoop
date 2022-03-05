608f46cae2f35699957d31a7cea56ae107548a9f
Fatime668 <tu7dkjdue@code.edu.az>
2022-03-05 08:36:09 +0400

    ConsoleLoop

---
+++ Loop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = { "Fatime", "Roya", "Aynura", "Gunel", "Qemer" };
+            bool result = false;
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (name[i].Length>5)
+                {
+                    Console.WriteLine(name[i]);
+                    result = true;
+                }
+                else
+                {
+
+                }
+            }
+            if (!result)
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
